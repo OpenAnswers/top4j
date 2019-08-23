4ab363f3c7fdba93bb37e39a83e7c98e874daf14
Ryan Young <ryan@openanswers.co.uk>
2019-08-23 11:43:37 +0100

    Add Open Answers website URL to copyright header.

--- top4j-javaagent/src/main/java/io/top4j/javaagent/mbeans/jvm/threads/HotMethods.java
+++ top4j-javaagent/src/main/java/io/top4j/javaagent/mbeans/jvm/threads/HotMethods.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Open Answers Ltd.
+ * Copyright (c) 2019 Open Answers Ltd. https://www.openanswers.co.uk
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
